7af83edf0b1d5bcdf253c37c418ec4d7ba668d16
Vinay Panchal <vinay.npanchal@gmail.com>
2024-07-23 14:52:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ import utime
 led = machine.Pin('LED', machine.Pin.OUT)
 while True:
     led.value(1)
-    utime.sleep(1)
+    utime.sleep(0.1)
     led.value(0)
-    utime.sleep(1)
+    utime.sleep(0.1)
