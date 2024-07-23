7a1f33fe05ee23750366dc4e14b97b6be3a89ed3
Vinay Panchal <vinay.npanchal@gmail.com>
2024-07-23 14:25:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://github.com/kevinmcaleer/ota_test/"
+firmware_url = "https://github.com/vinaynpp/ota/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
